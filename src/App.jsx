572c1942cdbc41b1debd969b5cc9412a170070ff
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-10-06 21:40:40 +0600

    first form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import SimpleFrom from './components/simpleFrom/SimpleFrom'
 
 function App() {
   
   return (
     <>
       <h1>Explore react from</h1>
+      <SimpleFrom></SimpleFrom>
     </>
   )
 }
